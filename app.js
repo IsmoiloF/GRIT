16a1c600fc2161f21d46de765e5b77918b8b8992
IsmoiloF <shoxruhismoilov5836@gmail.com>
2023-08-09 23:43:18 +0500

    Add change language

--- app.js
+++ app.js
@@ -2,21 +2,32 @@ function changeLanguage(lang) {
     location.hash = lang;
     location.reload();
   }
+ 
 
-  // Define the language reload anchors
   var language = {
     eng: {
       welcome: " GRIT sizning ishonchli texnik hamkoringizdir:"+
      "Ishingizning texnik tomoni haqida gap ketganda, GRIT yordam berish uchun shu yerda.  Bizning maqsadimiz sizga SMM, veb-sayt dizayni va yaratish sohasida sifatli va ishonchli yechimlarni taqdim etishdir.  Bugun hamkorlikni boshlash va kompaniyangizni yaxshilash uchun biz bilan bog'laning!",
-  mavzu:"Biz haqimizda"
-
+  mavzu:"Biz haqimizda",
+  logo:"GRIT",
+  header_1:"Asosiy",
+  header_2:"Biz haqimizda",
+  header_3:"Xizmatlarimiz",
+  header_4:"Tariflarimiz",
+  header_5:"Biz bilan bog'lanish" 
     },
     es: {
       welcome: "GRIT - ваш надежный технический партнер:"+
 "Когда дело доходит до технической части вашего бизнеса, компания GRIT готова вам помочь. Наша цель"+
  "- предоставить вам качественные и надежные решения в области SMM, дизайна и создания веб-сайтов. "+
  "Не стесняйтесь обращаться к нам, чтобы начать сотрудничество и улучшить вашу компанию уже сегодня!",
- mavzu:"О нас "
+ mavzu:"О нас ",
+ logo:"ГРИТ",
+ header_1:"Основной",
+ header_2:"О нас",
+ header_3:"Наши сервисы",
+ header_4:"Наши цены",
+ header_5:"Cвязаться с нами"
   }
 }
 
@@ -25,13 +36,29 @@ function changeLanguage(lang) {
         bizhaqimizdatext.textContent =
         language.es.welcome;
         bizhaqimizda.textContent=
-        language.es.mavzu
-
+        language.es.mavzu;
+        logo.textContent=language.es.logo;
+        header_1.textContent= language.es.header_1;
+        header_2.textContent= language.es.header_2;
+        header_3.textContent= language.es.header_3;
+        header_4.textContent= language.es.header_4;
+        header_5.textContent= language.es.header_5;
     }
     else if (window.location.hash == "#eng") {
         bizhaqimizdatext.textContent =
         language.eng.welcome;
         bizhaqimizda.textContent=
-        language.eng.mavzu
+        language.eng.mavzu;
+        logo.textContent=language.eng.logo;
+        header_1.textContent= language.eng.header_1;
+        header_2.textContent= language.eng.header_2;
+        header_3.textContent= language.eng.header_3;
+        header_4.textContent= language.eng.header_4;
+        header_5.textContent= language.eng.header_5;
     }
   }
+
+
+
+
+  
